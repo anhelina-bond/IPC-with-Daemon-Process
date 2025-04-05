73d0687a9007b795255dafe87c64cf9acd6b2084
anhelina-bond <124795789+anhelina-bond@users.noreply.github.com>
2025-04-05 12:17:33 +0300

    files fix

--- main.c
+++ main.c
@@ -181,6 +181,8 @@ void child_process2() {
 }
 
 
+// ... [keep all the includes and defines] ...
+
 int main(int argc, char *argv[]) {
     if (argc != 3) {
         fprintf(stderr, "Usage: %s <num1> <num2>\n", argv[0]);
@@ -191,49 +193,57 @@ int main(int argc, char *argv[]) {
     int num2 = atoi(argv[2]);
 
     // Create FIFOs
+    unlink(FIFO1);  // Remove if they exist
+    unlink(FIFO2);
+    
     if (mkfifo(FIFO1, 0666) == -1) {
-        if (errno != EEXIST) {
-            perror("mkfifo FIFO1");
-            exit(EXIT_FAILURE);
-        }
-    } else {
-        printf("fifo1 created successfully\n");
+        perror("mkfifo FIFO1");
+        exit(EXIT_FAILURE);
     }
-    if (mkfifo(FIFO2, 0666) < 0 && errno != EEXIST) {
+    printf("fifo1 created successfully\n");
+    
+    if (mkfifo(FIFO2, 0666) < 0) {
         perror("mkfifo FIFO2");
         unlink(FIFO1);
         exit(EXIT_FAILURE);
-    }else {
-        printf("fifo2\n");
+    }
+    printf("fifo2 created successfully\n");
+
+    // Create the daemon process
+    pid_t daemon_pid = fork();
+    if (daemon_pid == 0) {
+        if (become_daemon() == -1) {
+            perror("Failed to create daemon");
+            exit(EXIT_FAILURE);
+        }
+
+        struct sigaction dsa;
+        dsa.sa_handler = daemon_signal_handler;
+        sigemptyset(&dsa.sa_mask);
+        dsa.sa_flags = SA_RESTART;
+        sigaction(SIGUSR1, &dsa, NULL);
+        sigaction(SIGHUP, &dsa, NULL);
+        sigaction(SIGTERM, &dsa, NULL);
+
+        while (1) {
+            sleep(5);
+        }
     }
 
-    int fd1 = open(FIFO1, O_WRONLY);
-
-    if (fd1!=-1) {
-        int num[2] = {num1, num2};
-        write(fd1, num, sizeof(num));
-        close(fd1);
-    } else{
-        perror("Error opening FIFO1 in write mode");
-        exit(EXIT_FAILURE);
-    }
-
-    // Set up SIGCHLD handler
+    // Set up SIGCHLD handler first
     struct sigaction sa;
     sa.sa_handler = sigchld_handler;
     sigemptyset(&sa.sa_mask);
-    sa.sa_flags = SA_RESTART ; // Ensure handler runs for each child
+    sa.sa_flags = SA_RESTART;
     if (sigaction(SIGCHLD, &sa, NULL) < 0) {
         perror("sigaction");
         unlink(FIFO1);
         unlink(FIFO2);
         exit(EXIT_FAILURE);
     }
+    printf("SIGCHLD handler assigned\n");
 
-
-    printf("sigchild assigned\n");
-
-    // Create two child processes
+    // Create child processes first
     pid_t child1 = fork();
     if (child1 == -1) {
         perror("fork failed for child1");
@@ -241,7 +251,7 @@ int main(int argc, char *argv[]) {
         unlink(FIFO2);
         exit(EXIT_FAILURE);
     } else if (child1 == 0) {
-        child_process1();
+        child_process1();  // This will block waiting for FIFO1 to be opened for writing
     }
 
     pid_t child2 = fork();
@@ -251,44 +261,42 @@ int main(int argc, char *argv[]) {
         unlink(FIFO2);
         exit(EXIT_FAILURE);
     } else if (child2 == 0) {
-        child_process2();
+        child_process2();  // This will sleep first
     }
+
     printf("Parent process started. Child1 PID: %d, Child2 PID: %d\n", child1, child2);
     fflush(stdout);
 
-
-    // Create the daemon process after forking children
-    pid_t daemon_pid = fork();
-    if (daemon_pid == 0) {
-        if (become_daemon() == -1) {
-            perror("Failed to create daemon");
-            exit(EXIT_FAILURE);
-        }
-
-        // Register signal
-        struct sigaction sa;
-        sa.sa_handler = daemon_signal_handler;
-        sigemptyset(&sa.sa_mask);
-        sa.sa_flags = SA_RESTART;
-        sigaction(SIGUSR1, &sa, NULL);
-        sigaction(SIGHUP, &sa, NULL);
-        sigaction(SIGTERM, &sa, NULL);
-
-        while (1) {
-            sleep(5);
-        }
+    // Now open FIFO1 for writing (after child1 is ready to read)
+    int fd1 = open(FIFO1, O_WRONLY);
+    if (fd1 == -1) {
+        perror("Error opening FIFO1 in write mode");
+        unlink(FIFO1);
+        unlink(FIFO2);
+        exit(EXIT_FAILURE);
     }
+
+    int num[2] = {num1, num2};
+    if (write(fd1, num, sizeof(num)) == -1) {
+        perror("Error writing to FIFO1");
+        close(fd1);
+        unlink(FIFO1);
+        unlink(FIFO2);
+        exit(EXIT_FAILURE);
+    }
+    close(fd1);
+
+    
     total_children = 2;
 
-    // Main process proceeds normally
     printf("Main process PID: %d, entering main loop\n", getpid());
 
     while (child_count < total_children) {
-        printf("proceeding\n");
+        printf("proceeding...\n");
         sleep(2);
     }
 
-    write(STDOUT_FILENO, "All children have exited. Parent terminating.\n", 47);
+    printf("All children have exited. Parent terminating.\n");
     unlink(FIFO1);
     unlink(FIFO2);
     exit(EXIT_SUCCESS);
